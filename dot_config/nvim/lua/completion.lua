76edbaad57353bb485365b7155207fafb3dee25b
Marvin Beckers <mail@embik.me>
2022-04-19 08:04:39 +0200

    do not use buffer for main autocompletion

--- dot_config/nvim/lua/completion.lua
+++ dot_config/nvim/lua/completion.lua
@@ -19,7 +19,6 @@ cmp.setup({
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
-        { name = 'buffer' },
         { name = 'path' },
     }),
     formatting = {
