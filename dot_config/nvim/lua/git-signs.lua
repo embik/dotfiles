08ebbc42e4a0208834326b567fb4548b165fd774
Marvin Beckers <marvin@kubermatic.com>
2024-08-05 09:21:58 +0200

    nvim: update gitsigns and which-ke configuration
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>

--- dot_config/nvim/lua/git-signs.lua
+++ dot_config/nvim/lua/git-signs.lua
@@ -1,10 +1,21 @@
+vim.api.nvim_set_hl(0, 'GitSignsAdd', { link = 'DiffAdd' })
+vim.api.nvim_set_hl(0, 'GitSignsAddNr', { link = 'GitSignsAddNr' })
+vim.api.nvim_set_hl(0, 'GitSignsChange', { link = 'DiffChange' })
+vim.api.nvim_set_hl(0, 'GitSignsChangeNr', { link = 'GitSignsChangeNr' })
+vim.api.nvim_set_hl(0, 'GitSignsChangedelete', { link = 'DiffChange' })
+vim.api.nvim_set_hl(0, 'GitSignsChangedeleteNr', { link = 'GitSignsChangeNr' })
+vim.api.nvim_set_hl(0, 'GitSignsDelete', { link = 'DiffDelete' })
+vim.api.nvim_set_hl(0, 'GitSignsDeleteNr', { link = 'GitSignsDeleteNr' })
+vim.api.nvim_set_hl(0, 'GitSignsTopdelete', { link = 'DiffDelete' })
+vim.api.nvim_set_hl(0, 'GitSignsTopdeleteNr', { link = 'GitSignsDeleteNr' })
+
 require("gitsigns").setup {
     signs = {
-        add = {hl = "DiffAdd", text = "▌", numhl = "GitSignsAddNr"},
-        change = {hl = "DiffChange", text = "▌", numhl = "GitSignsChangeNr"},
-        delete = {hl = "DiffDelete", text = "_", numhl = "GitSignsDeleteNr"},
-        topdelete = {hl = "DiffDelete", text = "‾", numhl = "GitSignsDeleteNr"},
-        changedelete = {hl = "DiffChange", text = "~", numhl = "GitSignsChangeNr"}
+        add = { text = "▌" },
+        change = { text = "▌" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" }
     },
     numhl = false,
     watch_gitdir = {
