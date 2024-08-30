a5921ead62439095b1dd6a91520d162ef5b83ff8
Marvin Beckers <marvin@kubermatic.com>
2024-08-30 08:33:31 +0200

    nvim: add tabline to show buffers
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -72,6 +72,16 @@ return {
             theme = 'auto',
             globalstatus = true,
           },
+                tabline = {
+                  lualine_a = {
+                    'buffers',
+                },
+                  lualine_b = {},
+                  lualine_c = {},
+                  lualine_x = {},
+                  lualine_y = {},
+                  lualine_z = {'tabs'}
+                },
         },
     },
 }
